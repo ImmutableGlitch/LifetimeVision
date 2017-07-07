b105ab2bc5188299afd87e0d0d7ef619c88cdba2
Chris Caldwell <ccaldwell488@gmail.com>
2017-07-08 00:25:37 +0100

    initial commit

---
+++ projWeekVisualiser/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace projWeekVisualiser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtLife = new System.Windows.Forms.TextBox();
+            this.btnBegin = new System.Windows.Forms.Button();
+            this.pan = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Current Age";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(212, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Life Expectancy";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(106, 14);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 26);
+            this.txtAge.TabIndex = 4;
+            // 
+            // txtLife
+            // 
+            this.txtLife.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLife.Location = new System.Drawing.Point(339, 14);
+            this.txtLife.Name = "txtLife";
+            this.txtLife.Size = new System.Drawing.Size(100, 26);
+            this.txtLife.TabIndex = 5;
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBegin.Location = new System.Drawing.Point(445, 9);
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(87, 37);
+            this.btnBegin.TabIndex = 6;
+            this.btnBegin.Text = "Begin";
+            this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // pan
+            // 
+            this.pan.Location = new System.Drawing.Point(12, 52);
+            this.pan.Name = "pan";
+            this.pan.Size = new System.Drawing.Size(520, 800);
+            this.pan.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 865);
+            this.Controls.Add(this.pan);
+            this.Controls.Add(this.btnBegin);
+            this.Controls.Add(this.txtLife);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "How many weeks are left?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtLife;
+        private System.Windows.Forms.Button btnBegin;
+        private System.Windows.Forms.Panel pan;
+    }
+}
+
---
+++ projWeekVisualiser/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace projWeekVisualiser
+{
+    public partial class Form1 : Form
+    {
+        int x = 0;
+        int y = 0;
+
+        /* Get the age and life expectancy of the user
+         * get the current week of the year
+         * print red squares up to current week
+         * then print green squares up to life expectancy
+         */
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBegin_Click(object sender , EventArgs e)
+        {
+
+            // Reset the panel for repainting
+            x = 0;
+            y = 0;
+            Graphics g = pan.CreateGraphics();
+            g.Clear(pan.BackColor);
+            g.Dispose();
+
+            // Store the user age, lifetime expectancy,
+            // current week and amount of weeks passed to date
+            var currentCulture = CultureInfo.CurrentCulture;
+            var currentWeek = currentCulture.Calendar.GetWeekOfYear(
+                            DateTime.Now ,
+                            currentCulture.DateTimeFormat.CalendarWeekRule ,
+                            currentCulture.DateTimeFormat.FirstDayOfWeek);
+
+            var age = Convert.ToInt32(txtAge.Text);
+            var life = Convert.ToInt32(txtLife.Text);
+            var weeksPassed = (52 * age - 1) + currentWeek;
+
+            // Print all the weeks from birth to now
+            printSquares(Color.FromArgb(239 , 97 , 97) , 1 , weeksPassed);
+            // Print all the weeks from next week till death
+            printSquares(Color.FromArgb(97 , 239 , 97) , weeksPassed + 1 , 52 * life);
+        }
+
+        private void printSquares(Color c , int start , int end)
+        {
+            SolidBrush b = new SolidBrush(c);
+            Graphics g = pan.CreateGraphics();
+            // Size of square to represent week
+            int w = 8;
+            int h = 8;
+            int margin = w + 2;
+
+            for (int i = start; i <= end; i++)
+            {
+                g.FillRectangle(b , x , y , w , h);
+
+                x = x + margin;
+                // Begin print on 'new line' after a year
+                if (i % 52 == 0)
+                {
+                    x = 0;
+                    y = y + margin;
+                }
+            }
+
+            b.Dispose();
+            g.Dispose();
+        }
+    }
+}
